a4ac6df137509af629a066440f1902ea8a0f102a
明伊帆 <mingyifan1@mingyifandeMacBook-Pro.local>
2015-07-17 09:17:50 +0800

    Initial Commit

---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by 明伊帆 on 15/7/17.
+//  Copyright (c) 2015年 rimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitDemo
+//
+//  Created by 明伊帆 on 15/7/17.
+//  Copyright (c) 2015年 rimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
